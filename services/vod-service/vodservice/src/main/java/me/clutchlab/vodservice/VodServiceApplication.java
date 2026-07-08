672b8e441c22b6450b286a593af987bf147bd52d
Robert DeVivio <robertndevivio@outlook.com>
2026-07-08 01:37:51 -0400

    add vod service

---
+++ services/vod-service/vodservice/src/main/java/me/clutchlab/vodservice/VodServiceApplication.java
@@ -0,0 +1,12 @@
+package me.clutchlab.vodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodServiceApplication.class, args);
+    }
+}
---
+++ services/vod-service/vodservice/src/test/java/me/clutchlab/vodservice/VodserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.clutchlab.vodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
